c44aea72cfd3831ba87a01c7368ea567421f75f4
Nahian Rifaat <nrifaat26@gmail.com>
2023-03-10 11:59:20 +0600

    Added Surveys to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :surveys
   resources :survey_tokens
-  resources :survey_projects
+  resources :survey_projects do
+    resources :surveys
+  end
+  resources :surveys
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
